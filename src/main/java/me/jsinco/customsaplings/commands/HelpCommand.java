8886f4e1b9cf60aac43da67c777fc65983f85c6f
Jsinco <jonahmarksinclair@gmail.com>
2023-09-16 01:02:33 -0400

    Revisions

--- src/main/java/me/jsinco/customsaplings/commands/HelpCommand.java
+++ src/main/java/me/jsinco/customsaplings/commands/HelpCommand.java
@@ -16,12 +16,12 @@ public class HelpCommand implements SubCommand {
         }
 
         String helpMessage = TextUtils.colorcode("""
-                &#9ad36e&lC&#9ad671&lu&#99d974&ls&#99dd77&lt&#99e07a&lo&#98e37d&lm&#98e680&lS&#98ea82&la&#98ed85&lp&#97f088&ll&#97f38b&li&#97f78e&ln&#96fa91&lg&#96fd94&ls &rHelp
-                &e/customsaplings help &7- Displays this help message
-                &e/customsaplings paste &7- Pastes a schematic at the block you are looking at
-                &e/customsaplings give <?player> &7- Gives you a custom sapling ? gives to another player if specified
-                &e/customsaplings reload &7- Reloads the plugin
+                &#a8ff92/customsaplings help &#E2E2E2- Displays this help message
+                &#a8ff92/customsaplings paste &#E2E2E2- Pastes a schematic at the block you are looking at
+                &#a8ff92/customsaplings give <?player> &#E2E2E2- Gives you a custom sapling ? gives to another player if specified
+                &#a8ff92/customsaplings reload &#E2E2E2- Reloads the plugin
                 """);
+        sender.sendMessage(TextUtils.colorcode("&#9ad36e&lC&#9ad671&lu&#99d974&ls&#99dd77&lt&#99e07a&lo&#98e37d&lm&#98e680&lS&#98ea82&la&#98ed85&lp&#97f088&ll&#97f38b&li&#97f78e&ln&#96fa91&lg&#96fd94&ls &#E2E2E2v" + plugin.getDescription().getVersion() + " by " + plugin.getDescription().getAuthors().get(0)));
         sender.sendMessage(helpMessage);
     }
 
