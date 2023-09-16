f1f34fbeeee5d44571106e09be80693750f04b96
Jsinco <jonahmarksinclair@gmail.com>
2023-09-16 00:50:11 -0400

    Updates to give command

--- src/main/java/me/jsinco/customsaplings/commands/GiveCommand.java
+++ src/main/java/me/jsinco/customsaplings/commands/GiveCommand.java
@@ -4,6 +4,7 @@ import me.jsinco.customsaplings.CustomSaplings;
 import me.jsinco.customsaplings.FileManager;
 import me.jsinco.customsaplings.Saplings;
 import me.jsinco.customsaplings.util.TextUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -24,10 +25,28 @@ public class GiveCommand implements SubCommand {
         }
 
         ItemStack sapling = Saplings.getSapling(args[1]);
-        if (sapling != null) {
-            player.getInventory().addItem(sapling);
-        } else {
+        if (sapling == null) {
             player.sendMessage(TextUtils.prefix + "That sapling does not exist!");
+            return;
+        }
+
+        Player deliverTo = args.length == 3 ? Bukkit.getPlayerExact(args[2]) : player;
+        if (deliverTo == null) {
+            player.sendMessage(TextUtils.prefix + "That player is not online!");
+            return;
+        }
+
+        for (int i = 0; i < 36; i++) {
+            if (deliverTo.getInventory().getItem(i) == null || deliverTo.getInventory().getItem(i).equals(sapling)) {
+                deliverTo.getInventory().addItem(sapling);
+                break;
+            } else if (i == 35) {
+                deliverTo.getWorld().dropItem(player.getLocation(), sapling);
+            }
+        }
+
+        if (!deliverTo.equals(player)) {
+            player.sendMessage(TextUtils.prefix + deliverTo.getName() + " was given a: " + args[1] + " sapling");
         }
     }
 
--- src/main/java/me/jsinco/customsaplings/commands/HelpCommand.java
+++ src/main/java/me/jsinco/customsaplings/commands/HelpCommand.java
@@ -14,7 +14,7 @@ public class HelpCommand implements SubCommand {
                 &#9ad36e&lC&#9ad671&lu&#99d974&ls&#99dd77&lt&#99e07a&lo&#98e37d&lm&#98e680&lS&#98ea82&la&#98ed85&lp&#97f088&ll&#97f38b&li&#97f78e&ln&#96fa91&lg&#96fd94&ls &rHelp
                 &e/customsaplings help &7- Displays this help message
                 &e/customsaplings paste &7- Pastes a schematic at the block you are looking at
-                &e/customsaplings give &7- Gives you a custom sapling
+                &e/customsaplings give <?player> &7- Gives you a custom sapling ? gives to another player if specified
                 &e/customsaplings reload &7- Reloads the plugin
                 """);
         sender.sendMessage(helpMessage);
